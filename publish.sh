76f24f6182ce9d06410dadac164f63d9e1f9f2cd
Jayden M <drjaydenm@hotmail.com>
2023-05-12 17:44:54 +1000

    Updated the publish script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ BUCKET_NAME=jaydenm.com
 
 echo "Bucket Name: $BUCKET_NAME"
 echo "Running S3 sync in dry-run mode..."
-aws s3 sync public/ s3://$BUCKET_NAME --exclude ".DS_Store" --delete --dryrun
+aws s3 sync public/ s3://$BUCKET_NAME --exclude ".DS_Store" --delete --cache-control 'max-age=86400' --dryrun
 
 if ! $SKIP_CONFIRM
 then
@@ -17,7 +17,11 @@ then
 fi
 
 echo "Running the real sync..."
-aws s3 sync public/ s3://$BUCKET_NAME --exclude ".DS_Store" --delete
+aws s3 sync public/ s3://$BUCKET_NAME --exclude ".DS_Store" --exclude "*.html" --delete --cache-control 'max-age=86400'
+
+echo "Disabling caching on HTML files..."
+aws s3 sync public/ s3://$BUCKET_NAME --exclude "*" --include "*.html" --cache-control 'max-age=0'
+
 echo "Sync complete"
 
 echo "Finding CloudFront distribution ID for Origin $BUCKET_NAME"
